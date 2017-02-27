b93d4a02d4dbb076cff1cd00e535e7ef45e9e7a4
Joran Rood <joran.rood@me.com>
2017-02-27 21:43:57 +0100

    Implement dice evaluation method for the local player
    The local player is also shown the probability of the current claim
    being true. This will likely become a configurable feature, as it might
    be desirable to increase the difficulty level and hide the probability.

--- src/dicebluff-client/app/local-player.js
+++ src/dicebluff-client/app/local-player.js
@@ -23,7 +23,57 @@ LocalPlayer.prototype.evaluateClaim = function(
     wildcardPermitted,
     verdictCallback
 ) {
-    //
+    var claimEyesCount = claimEyesList.length,
+        handEyesCount = handEyesList.length,
+        matchedEyesCount,
+        claimProbability,
+        claimDiceGroup,
+        handDiceGroup;
+
+    // Determine how many of the claim eyes are in our hand
+    matchedEyesCount = DiceFilter.matchEyes(
+        handEyesList,
+        claimEyesList,
+        wildcardPermitted,
+        null,
+        null
+    );
+
+    claimProbability = DiceStatistics.lookupPartialProbability(
+        totalDiceCount,
+        handEyesCount,
+        claimEyesCount,
+        matchedEyesCount,
+        wildcardPermitted
+    );
+
+    this.console.createOutput('The claim:', false);
+
+    claimDiceGroup = this.console.createDiceGroup(
+        Dice.smallFrames,
+        claimEyesCount
+    );
+
+    claimDiceGroup.drawFrame(claimEyesList);
+
+    this.console.createEmptyOutput(false);
+    this.console.createOutput('Your hand:', false);
+
+    handDiceGroup = this.console.createDiceGroup(
+        Dice.smallFrames,
+        handEyesCount
+    );
+
+    handDiceGroup.drawFrame(handEyesList);
+
+    this.console.createEmptyOutput(false);
+
+    this.console.createOutput('The claim has a '
+        + Math.round((100 * claimProbability))
+        + '% probability of being true');
+
+    this.console.createOutput('Do you accept this claim?', false);
+    this.console.promptForConfirmation(verdictCallback, true);
 };
 
 LocalPlayer.prototype.reviseClaim = function(
