6b14d5f10c1550c43530484a73508ea87691d47e
Joran Rood <joran.rood@me.com>
2017-02-27 21:45:00 +0100

    Add testing code for the local player evaluation method

--- src/dicebluff-client/app/main.js
+++ src/dicebluff-client/app/main.js
@@ -1,5 +1,9 @@
 ;(function(windowReference, documentReference) {
     'use strict';
 
-    //
+    var consoleNode = documentReference.querySelector('.console'),
+        console = new Console(consoleNode, 72, 18, (18 * 10)),
+        localPlayer = new LocalPlayer(console);
+
+    localPlayer.evaluateClaim(40, [ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 ], [ 1, 2, 3 ], true, function() {});
 })(window, document);
